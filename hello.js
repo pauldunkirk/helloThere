c90a533720f8c15f09f3b383ac66b2af6ae8be99
Paul C Dunkirk <paulcdunkirk@Pauls-MacBook-Air.local>
2017-01-11 10:57:07 -0600

    initial commit

---
+++ hello.js
@@ -0,0 +1,34 @@
+console.log("test");
+var numOfCustomers = prompt("How many customers are there?");
+while (numOfCustomers > 0) {
+alert( "How may I help you?" );
+
+// help the customer...
+
+numOfCustomers = numOfCustomers - 1;
+}
+var name = prompt("Who you?");
+
+var sup = prompt("Hi " + name + "!  Sup?");
+
+next = prompt(sup + "? That's sweet! What else " +name+ "?");
+
+var next = prompt(next + "? That's so goofy!");
+
+alert("Later Guv'na!");
+
+var clicks=0;
+var tau = 6.28318;
+var addClick = function(){
+  clicks++;
+  console.log(clicks + " click\(s\). Simply comparing clicks to tau, which is 6.28318 but rounding to 6 when we click of course. When you get to 7, console will log \"true\"");
+  console.log(isGreaterThan( clicks, tau) );
+} //end addClick
+
+var isGreaterThan = function (number0, number1) {
+  if (number0 > number1){
+    return true;
+  } else {
+    return false;
+  }
+}
